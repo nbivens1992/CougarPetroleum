c04c96c8c2380cfcdbe4d8b4f0b9525b887ac7a3
Corey Dillard <coreydillard123@gmail.com>
2023-04-02 14:22:49 -0500

    corey's changes

---
+++ JestApp/test.js
@@ -0,0 +1,9 @@
+const user = require('../users');
+
+describe("Tests", () => {
+    test("Adding new user", () => {
+        var result = user.addNewUser( "testuser", "1234gf", "test name", "test address", "test address2", "Katy", "TX", "test zip" )
+    });
+
+
+})
