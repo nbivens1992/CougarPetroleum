0b7603ec804f5555849fd89b8b32c8f557d23ba5
Corey Dillard <coreydillard123@gmail.com>
2023-04-02 16:43:33 -0500

    corey's changes

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router-dom'
+import signUpForm from './signUpForm.html'
+
+function App() {
+  return (
+    <Routes>
+        <Route path="signUpForm" element={<signUpForm />} >
+        </Route>
+    </Routes>
+  )
+}
+
+export default App;
